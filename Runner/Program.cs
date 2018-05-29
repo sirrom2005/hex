56b7eb7a1be216b9a5537b2dbc9004bd25af7797
Rohan Morris <rohan.morris@vmbs.com>
2018-05-29 01:33:43 -0500

    Add project files.

---
+++ Runner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using rmCrypt; 
+
+namespace Runner
+{
+    class Program
+    {
+        private static Crypt c = new Crypt("aaasada");
+
+        static void Main(string[] args)
+        {
+            string text = "The brown jox jump over the tree";
+            var en = c.EnCode(text);
+            var dn = c.DeCode(en);
+
+
+            Console.WriteLine($"Input >>{text}");
+            Console.WriteLine($"Encoded String>>>\n{en}");
+            Console.WriteLine($"Decoded String>>> {dn}");
+            Console.Read();
+        }
+    }
+}
---
+++ rmCrypt/Crypt.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rmCrypt
+{
+    public class Crypt
+    {
+        private string key;
+
+        public Crypt(string key)
+        {
+            this.key = key;
+        }
+
+        public string EnCode(string value)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(value);
+            string hex = BitConverter.ToString(bytes);
+            return hex.Replace("-","::0x");
+        }
+
+        public string DeCode(string value)
+        {
+            string output = "";
+            value = value.Replace("::0x", "-");
+            int len = value.Length;
+
+            int[] _byte = new int[len];
+            byte[] data = new byte[len];
+
+            int i = 0;
+            foreach(string x in value.Split('-')) {
+                int b = Convert.ToInt32(x, 16);
+                output = output + Char.ConvertFromUtf32(b);
+                i++;
+            }
+
+            return output;
+        }
+    }
+}
